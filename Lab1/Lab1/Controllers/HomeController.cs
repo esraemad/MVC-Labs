319c88bfc573243fdd4f024096e2f432516b2eaa
Esra Emad <esso.em.96@gmail.com>
2020-05-04 03:41:00 +0200

    first commit

---
+++ Lab1/Lab1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+
+    }
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        // GET: RegisterForm
+        [HttpGet]
+        public ViewResult RegistrationForm()
+        {
+            return View();
+        }
+
+        // Post: RegisterForm
+        [HttpPost]
+        public ViewResult RegistrationForm(Employee employee)
+        {
+            if (employee != null && employee.Email != null && employee.Name != null)
+            {
+                ViewBag.Name = employee.Name;
+                return View("Welcome");
+            }
+            return View();
+        }
+    }
+}
